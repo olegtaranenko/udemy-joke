8117c0697dbc7bd12688c14ce5a82e4e5ac8ab57
Oleg Taranenko MP <olegtaranenko@gmail.com>
2019-03-03 12:32:59 +0100

    joke app working as it should

--- src/main/java/com/olegtaranenko/udemy/jokeapp/services/JokeService.java
+++ src/main/java/com/olegtaranenko/udemy/jokeapp/services/JokeService.java
@@ -1,8 +1,7 @@
 package com.olegtaranenko.udemy.jokeapp.services;
 
-import org.springframework.stereotype.Service;
 
-@Service
+
 public interface JokeService {
     String getJoke();
 }
--- src/main/java/com/olegtaranenko/udemy/jokeapp/services/JokeServiceImpl.java
+++ src/main/java/com/olegtaranenko/udemy/jokeapp/services/JokeServiceImpl.java
@@ -1,16 +1,16 @@
 package com.olegtaranenko.udemy.jokeapp.services;
 import guru.springframework.norris.chuck.ChuckNorrisQuotes;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+@Service
 public class JokeServiceImpl implements JokeService {
-    private ChuckNorrisQuotes quotes;
+    private final ChuckNorrisQuotes quotes;
 
-    @Autowired
-    public JokeServiceImpl(ChuckNorrisQuotes quotes) {
-        this.quotes = quotes;
+    public JokeServiceImpl() {
+        this.quotes = new ChuckNorrisQuotes();
     }
 
+    @Override
     public String getJoke() {
         return quotes.getRandomQuote();
     }
