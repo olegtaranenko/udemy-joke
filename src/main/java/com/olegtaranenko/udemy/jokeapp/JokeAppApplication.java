e85d98426dffe05464299377248deccb3a0cb28b
Oleg Taranenko MP <olegtaranenko@gmail.com>
2019-03-03 12:45:53 +0100

    xml configuration

--- src/main/java/com/olegtaranenko/udemy/jokeapp/JokeAppApplication.java
+++ src/main/java/com/olegtaranenko/udemy/jokeapp/JokeAppApplication.java
@@ -2,8 +2,10 @@ package com.olegtaranenko.udemy.jokeapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource("chuck-config.xml")
 public class JokeAppApplication {
 
     public static void main(String[] args) {
